c7ef0bc5e64ecc36f961cf19ad6c5501e4301e42
hugoroca <hugo.rock20@hotmail.com>
2020-04-23 12:42:17 -0500

    revice all parameters

--- src/controllers/connect.controller.js
+++ src/controllers/connect.controller.js
@@ -4,6 +4,14 @@ const ConnectService = require('../services/connect.service')
 module.exports = class {
   async init (ctx) {
     const connectModel = new ConnectModel(
+      ctx.request.body.host,
+      ctx.request.body.sheme,
+      ctx.request.body.port,
+      ctx.request.body.enableLogging,
+      ctx.request.body.logger,
+      ctx.request.body.apiKeyId,
+      ctx.request.body.secretApiKey,
+      ctx.request.body.integrator,
       ctx.request.body.merchantId
     )
     try {
--- src/models/connectRequest.model.js
+++ src/models/connectRequest.model.js
@@ -1,23 +1,23 @@
 module.exports = class {
   constructor (
-    // host,
-    // sheme,
-    // port,
-    // enableLogging,
-    // logger,
-    // apiKeyId,
-    // secretApiKey,
-    // integrator,
+    host,
+    sheme,
+    port,
+    enableLogging,
+    logger,
+    apiKeyId,
+    secretApiKey,
+    integrator,
     merchantId
   ) {
-    // this.host = host
-    // this.sheme = sheme
-    // this.port = port
-    // this.enableLogging = enableLogging
-    // this.logger = logger
-    // this.apiKeyId = apiKeyId
-    // this.secretApiKey = secretApiKey
-    // this.integrator = integrator
+    this.host = host
+    this.sheme = sheme
+    this.port = port
+    this.enableLogging = enableLogging
+    this.logger = logger
+    this.apiKeyId = apiKeyId
+    this.secretApiKey = secretApiKey
+    this.integrator = integrator
     this.merchantId = merchantId
   }
 }
